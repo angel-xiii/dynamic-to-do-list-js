f37780fd1e5f604d640dcbf18c05dc4baa2f842d
angelnyambi <angel_nyambi@outlook.com>
2025-10-04 19:59:04 +0200

    Added button

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
+    const addButton = document.getElementById('add-task-btn');
+
     function addTask() {
         const taskText = taskInput.value.trim();
 
